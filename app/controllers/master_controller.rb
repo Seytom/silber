83d726f4ad0667dea53a910e0a1b7a888dc84bb2
Seytom <tj.seymour@yahoo.com>
2014-10-15 05:28:44 -0700

    Initial commit

---
+++ app/controllers/master_controller.rb
@@ -0,0 +1,22 @@
+class MasterController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def destroy
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def show
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Silber::Application.config.secret_key_base = 'fe8bafb2e942173416a4d06d58e1255e746064a48d6f0b1409ff5a8ec889b20c9f095bbdec9bcb3527bcb142984d9b6ecd27f954698bb33fdf914cd169104bb4'
